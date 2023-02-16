bfc3a32a95d36f8765b6e4f501b24beb14dfcdcc
rustamc <rchochaev@gmail.com>
2023-02-16 13:05:29 +0300

    Update Evaluation.py

--- GlobalRoutingRL/eval/Evaluation.py
+++ GlobalRoutingRL/eval/Evaluation.py
@@ -3,13 +3,9 @@ import os
 if __name__ == "__main__":
     benchmark_num = []
     for i in range(len(benchmark_num)):
-        command = "perl eval2008.pl test_benchmark_{num}.gr\
-        test_benchmark_{num}.gr.DRLsolution".format(num=benchmark_num[i])
+        command = "perl eval2008.pl ../benchmark_reduced/test_benchmark_{num}.gr\
+        ../solutionsDRL/test_benchmark_{num}.gr.DRLsolution".format(num=benchmark_num[i])
         os.system(command)
-        command = "perl eval2008.pl test_benchmark_{num}.gr\
-         test_benchmark_{num}.grAstar_solution".format(num=benchmark_num[i])
+        command = "perl eval2008.pl ../benchmark_reduced/test_benchmark_{num}.gr\
+        ../solutionsA_reduced/test_benchmark_{num}.grAstar_solution".format(num=benchmark_num[i])
         os.system(command)
-
-
-
-
